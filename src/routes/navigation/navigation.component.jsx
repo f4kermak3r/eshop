9845f74643ae5bc6b7af6ff48118dabc92ca8b85
tauras <ttauras20@gmail.com>
2022-09-12 19:59:00 +0300

    nav bar implementation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Fragment } from "react";
+
+import { Outlet, Link } from "react-router-dom";
+
+import logo from "../../assets/iconpear.png";
+
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img className="logo" src={logo} alt="logo"></img>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
