a74fb7c9eac7e5bc92d32320d2a6757bede19a84
tauras <ttauras20@gmail.com>
2022-09-12 18:04:39 +0300

    make categoriest item component and categories list component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
+import CategoriesList from "./components/categoriest-list/categories-list-component";
+
 const App = () => {
   const categories = [
     {
       id: 1,
       title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
       title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
       title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: "hoodies",
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "shoes",
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
+
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => {
-        return (
-          <div className="category-container">
-            <div className="image"></div>
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>shop</p>
-            </div>
-          </div>
-        );
-      })}
+      <CategoriesList categories={categories} />
     </div>
   );
 };
