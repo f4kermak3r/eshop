c82b6718cb1e75c039eed5ffc79bae09df11d9e8
tauras <ttauras20@gmail.com>
2022-09-30 12:07:56 +0300

    add payment form component

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,15 +1,30 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { BUTTON_TYPES_CLASSES } from "../button/button.component";
 
 import Button from "../button/button.component";
 
+import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+
 export const PaymentForm = () => {
-    return(
-        <div>
-            <CardElement/>
-            <Button buttonType={BUTTON_TYPES_CLASSES.inverted}>Pay now</Button>
-        </div>
-    )
-}
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+  };
+  return (
+    <PaymentFormContainer>
+      <FormContainer>
+        <h2>Credit card payment:</h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPES_CLASSES.inverted}>Pay now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
 
 export default PaymentForm;
