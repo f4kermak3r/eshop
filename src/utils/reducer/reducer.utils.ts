42f4885fd92459fc42acbfed7be1babb77408553
unknown <ttauras20@gmail.com>
2022-10-03 23:51:52 +0300

    initialize typescript instead of js in project

---
+++ src/utils/reducer/reducer.utils.ts
@@ -0,0 +1,22 @@
+import { typeOptions } from '@testing-library/user-event/dist/type/typeImplementation';
+import { AnyAction } from 'redux';
+
+export type ActionWithPayload<T, P> = {
+  type: T;
+  payload: P;
+}
+
+export type Action<T> = {
+  type: T;
+}
+
+export function createAction<T extends string, P>(type: T, payload: P): ActionWithPayload<T,P>;
+export function createAction<T extends string>(type: T, payload: void): Action<T>;
+
+export function createAction<T extends string, P>(type: T, payload: P) {
+  return { type, payload};
+}
+
+// export const createAction = (type, payload) => {
+//   return { type, payload };
+// };
